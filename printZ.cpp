28f1d6205118e64030ef20d3ea63bb96c8693aab
Anjali Chauhan <56559378+anjali1102@users.noreply.github.com>
2020-09-25 19:58:33 +0530

    Add files via upload

---
+++ printZ.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    for (int x = 0; x < n; x++)
+    {
+        cout << "*";
+    }
+    cout << endl;
+    for (int x = n - 3; x >= 0; x--)
+    {
+        for (int y = x; y >= 0; y--)
+        {
+            cout << " ";
+        }
+        cout << "*" << endl;
+    }
+    for (int x = 0; x < n; x++)
+    {
+        cout << "*";
+    }
+
+    return 0;
+}
